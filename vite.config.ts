16f70eb8574c21668ae01bd246ab6339e3107390
RJustn <rafaelricasatal@yahoo.com>
2025-02-18 01:20:36 +0800

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     exclude: ['bcrypt', '@mapbox/node-pre-gyp']
   },
   build: {
+    outDir: 'build',  // Change this if needed
     rollupOptions: {
       external: ['path', 'fs', 'events', 'util']
     }
