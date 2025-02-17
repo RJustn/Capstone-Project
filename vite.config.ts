a1a4dfdd8348b71b1c7f75a0477d6f906b07fc4a
RJustn <rafaelricasatal@yahoo.com>
2025-02-17 23:44:36 +0800

    crypto

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['path', 'crypto', 'fs', 'events', 'util']
+      external: ['path', 'fs', 'events', 'util']
     }
   }
 });
