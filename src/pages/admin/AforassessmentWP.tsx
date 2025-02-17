565f47d6b293199bd370c584e9ba473f7ca88e14
RJustn <rafaelricasatal@yahoo.com>
2025-02-17 21:50:41 +0800

    try

--- src/pages/admin/AforassessmentWP.tsx
+++ src/pages/admin/AforassessmentWP.tsx
@@ -1,4 +1,4 @@
-import '../styles/AdminStyles.css';
+import '../Styles/AdminStyles.css';
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Modal from 'react-modal';
--- src/pages/superadmin/superadminAccountEdit.tsx
+++ src/pages/superadmin/superadminAccountEdit.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import '../Styles/SuperAdminStyles.css';
 
 const SuperadminAccountEdit = () => {
