ae82cafe5932a76eb3821e0d3ec5dad78391e5f2
RJustn <rafaelricasatal@yahoo.com>
2025-02-18 01:59:54 +0800

    CORSTRY

--- backend/server.js
+++ backend/server.js
@@ -26,9 +26,11 @@ const io = socketIo(server);
 
 // Define CORS options
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://capstone-project-teal-three.vercel.app'], // Add your frontend URLs
-  credentials: true // Allow credentials (cookies, authorization headers)
+  origin: 'https://capstone-project-teal-three.vercel.app', // Allow your frontend domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
 };
+
 
 // Use CORS middleware
 app.use(cors(corsOptions));  // This line applies the CORS policy globally to all routes
