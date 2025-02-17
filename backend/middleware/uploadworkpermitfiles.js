b898ac439a17051cb1bee8e3039ab8aee3b4cbb1
RJustn <rafaelricasatal@yahoo.com>
2025-02-18 01:35:57 +0800

    commitng

--- backend/middleware/uploadworkpermitfiles.js
+++ backend/middleware/uploadworkpermitfiles.js
@@ -2,15 +2,11 @@ const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
 
-// Ensure the upload directory exists
-const uploadDir = path.join(__dirname, '../documents/uploads');
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
+
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, uploadDir); // Use absolute path
+    cb(null, '../documents/uploads'); // Use absolute path
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + '-' + file.originalname);
--- backend/middleware/uploadworkpermitreceipt.js
+++ backend/middleware/uploadworkpermitreceipt.js
@@ -2,15 +2,10 @@ const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
 
-// Ensure the upload directory exists
-const uploadDir = path.join(__dirname, '../documents/receipts');
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, uploadDir); // Use absolute path
+    cb(null, '../documents/receipts'); // Use absolute path
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + '-' + file.originalname);
