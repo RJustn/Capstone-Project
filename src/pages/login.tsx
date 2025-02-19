bed120383c286a38cd30a9ed5b61438bb05d8f95
RJustn <rafaelricasatal@yahoo.com>
2025-02-19 19:37:26 +0800

    again

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -21,7 +21,7 @@ const Login: React.FC = () => {
     }
   
     try {
-      const response = await fetch('http://localhost:3000/auth/login', {
+      const response = await fetch('https://capstone-project-backend-nu.vercel.app/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
