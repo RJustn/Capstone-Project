947dcc386e8ebd559d0945514dba46dfefd6c067
Danawl <awildandan@gmail.com>
2025-04-27 01:19:41 +0800

    fixing work information in working permits

--- backend/utils/generateWorkPermitPDF.js
+++ backend/utils/generateWorkPermitPDF.js
@@ -105,17 +105,27 @@ const generateWorkPermitPDF = async (id) => {
 
       // Add a new page
       doc.addPage({ size: 'A4', layout: 'landscape' });
+      currentY = doc.page.margins.top + 20;
       const centerX = doc.page.width / 2;
       doc.fontSize(15).text('Account Details', centerX, currentY, { align: 'center' });
       doc.moveDown(2);
-      currentY = doc.page.margins.top + 20;
       doc.text(`Permit ID: ${workPermit.id || 'N/A'}`, leftColumnX, currentY);
       currentY += 20;
       doc.text(`Permit Type: ${workPermit.permittype || 'N/A'}`, leftColumnX, currentY);
       currentY += 20;
       doc.text(`Classification: ${workPermit.classification || 'N/A'}`, leftColumnX, currentY);
       currentY += 20;
-      doc.text(`Amount to Pay: ${workPermit.classification === 'new' ? 0 : (workPermit.classification === 'renew' ? 200 : (workPermit.receipt?.amountPaid !== undefined ? workPermit.receipt?.amountPaid : 'N/A'))}`, leftColumnX, currentY);
+      doc.text(
+        `Amount to Pay: ${
+          workPermit.classification?.toLowerCase() === 'new'
+            ? 0
+            : workPermit.classification?.toLowerCase() === 'renew'
+            ? 200
+            : 'N/A'
+        }`,
+        leftColumnX,
+        currentY
+      );
       currentY += 20;
       doc.moveDown(4);
 
